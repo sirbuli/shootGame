667c1efec0bdd1ce1d0dc0e320a12fc7c45ff039
sirbuli <bulibulislaw@gmail.com>
2023-06-27 15:29:25 +0200

    Added news section

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,12 @@ import App from './controller.js';
 
 new App();
 console.log(typeof undefined);
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
